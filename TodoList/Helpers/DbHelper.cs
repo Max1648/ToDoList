31374f9b1d1ecdae4b2a3b929e44b9db134f00d9
Max Nadvornyi <ipzk201_nmyu@student.ztu.edu.ua>
2022-04-26 14:14:39 +0300

    ToDoList

---
+++ TodoList/Helpers/DbHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace TodoList.Helpers
+{
+    public class DbHelper
+    {
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=TodoList;");
+        }
+    }
+
+}
